6eea8b9f19320e5b585e44e616868a17d9569aa6
nusc2016 <61950417+nusc2016@users.noreply.github.com>
2020-06-25 08:57:56 -0700

    updated predictions

--- web_app/routes/prediction_routes.py
+++ web_app/routes/prediction_routes.py
@@ -2,6 +2,8 @@ from flask import Blueprint, render_template
 from flask import Flask, request, jsonify
 from airbnb_functions import preprocessing, xgboost_function, df_maker
 import requests
+import pandas as pd 
+
 
 
 prediction_routes = Blueprint('prediction_routes', __name__)
@@ -11,8 +13,8 @@ prediction_routes = Blueprint('prediction_routes', __name__)
 def prediction():
     request_data = request.get_json(force=True)
     print(type(request_data))
-    neighborhood_group = request_data["neighborhood_group"]
-    neighborhood =request_data["neighborhood"]
+    neighbourhood_group = request_data["neighbourhood_group"]
+    neighbourhood =request_data["neighbourhood"]
     latitude =request_data["latitude"]
     longitude =request_data["longitude"]
     room_type =request_data["room_type"]
@@ -21,19 +23,22 @@ def prediction():
     calculated_host_listing_count =request_data["calculated_host_listing_count"]
     availability_365 =request_data["availability_365"]
 
-    model = load(open("model.pkl", "rb"))
 
     df = pd.read_csv('AB_NYC_2019.csv')
 
     X_train_df, X_test_df, X_train, X_test, y_train, y_test = preprocessing(df)
+    pred = xgboost_function(X_train, y_train)
+
+    df = df_maker(y_train, pred)
 
 
-    return X_train_df.to_json(orient='records')
+    return df.to_json(orient='records')
+
 
 @prediction_routes.route('/test', methods=['GET'])
 def test():
-    neighborhood_group = "Staten Island" 
-    neighborhood = "Port Richmond"
+    neighbourhood_group = "Staten Island" 
+    neighbourhood = "Port Richmond"
     latitude = 40.615542
     longitude = -74.14331
     room_type = "Private room"
@@ -41,15 +46,11 @@ def test():
     number_of_reviews = 0
     calculated_host_listing_count = 0
     availability_365 = 60
-    post = {"neighborhood_group": neighborhood_group}
-    post = {"neighborhood": neighborhood}
-    post = {"latitude": latitude}
-    post = {"longitude": longitude}
-    post = {"room_type": room_type}
-    post = {"minimum_nights": minimum_nights}
-    post = {"number_of_reviews": number_of_reviews}
-    post = {"calculated_host_listing_count": calculated_host_listing_count}
-    post = {"availability_365": availability_365}
+    post = {"neighbourhood_group": neighbourhood_group, "neighbourhood": neighbourhood,
+            "latitude": latitude, "longitude": longitude, "room_type": room_type,
+            "minimum_nights": minimum_nights, "number_of_reviews": number_of_reviews,
+            "calculated_host_listing_count": calculated_host_listing_count,
+            "availability_365": availability_365}
 
     URL = "http://127.0.0.1:5000/prediction"
 
