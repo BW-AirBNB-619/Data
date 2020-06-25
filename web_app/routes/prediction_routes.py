cbbc481057ed6ca6bb25e3fcdb8154f26a6fe93a
nusc2016 <61950417+nusc2016@users.noreply.github.com>
2020-06-25 13:43:06 -0700

    take 2k

--- web_app/routes/prediction_routes.py
+++ web_app/routes/prediction_routes.py
@@ -30,7 +30,7 @@ def prediction():
                    number_of_reviews, calculated_host_listings_count, availability_365, df) 
 
     X_train, y_train = preprocessing(new_df)
-    model = rfr_function(X_train, y_train)
+    model = load(open("rfr_model.pkl", "rb"))
     prediction = predict(X_train, model)
     
     preddf = pd.DataFrame(prediction)
